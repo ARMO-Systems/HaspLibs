fe709b4a4b039fadb741a5d3a8564126c07116da
Podlesnyy Andrey <podlesniy@armo.ru>
2014-07-29 11:14:01 +0400

    1

---
+++ build.bat
@@ -0,0 +1,18 @@
+@echo On
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+cd Packs
+
+set firstPartNuget=%nuget% pack 
+set lastPartNuget= -NonInteractive %version%
+
+cmd /c %firstPartNuget%HaspLibs.nuspec%lastPartNuget%
+cmd /c %firstPartNuget%HaspGenerateLibs.nuspec%lastPartNuget%
